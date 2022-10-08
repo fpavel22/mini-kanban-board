35321e8faf6f249ed564bf715f30bb29bc19c05c
Florin-Iulian PAVEL <fpavel@pentalog.com>
2022-10-08 22:53:35 +0300

    Created navbar with light/dark theme customization

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import cn from 'classnames';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
-import { Button } from './components/button';
-import { TextField } from './components/text-field';
+import { Navbar } from './components/navbar';
 import { themeSliceSelector } from './features/themeSlice';
 import './styles/App.scss';
 
@@ -16,11 +15,7 @@ function App() {
 
   return (
     <main className={ _className }>
-      <Button type="primary" size="lg">Primary L</Button>
-      <Button type="secondary">Secondary</Button>
-      <Button type="danger">Danger</Button>
-      <TextField placeholder="Enter a text..." />
-      <TextField placeholder="Enter a text..." error={ true } />
+      <Navbar />
     </main>
   );
 }
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,5 +1,28 @@
-export const Navbar = () => {
+import cn from 'classnames';
+import { useSelector } from "react-redux";
+
+import { Button } from '../button';
+import { themeSliceSelector } from '../../features/themeSlice';
+import logoDark from '../../assets/logo-dark.svg';
+import logoLight from '../../assets/logo-light.svg';
+import './navbar.scss';
+
+export const Navbar = ({ className, ...props }) => {
+  const darkMode = useSelector(themeSliceSelector);
+
+  const _className = cn('header', {
+    'header--d-mode': darkMode
+  }, className);
+
   return (
-    <div>navbar</div>
+    <header { ...props } className={ _className }>
+      <div className="header__logo">
+        <img src={ darkMode ? logoLight : logoDark } alt="Header logo" />
+      </div>
+      <div className="header__informative">
+        <h2>Platform Launch</h2>
+        <Button type="primary" size="lg">+ Add New Task</Button>
+      </div>
+    </header>
   );
 }
