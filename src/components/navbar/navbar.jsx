ebac5dae92f42f051c745734527b5d8142a1a001
Florin Pavel <vampirism2356@gmail.com>
2023-05-06 11:46:25 +0300

    Minor changes to media queries and SASS variables

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -39,15 +39,13 @@ export const Navbar = ({ className }) => {
     }
   };
 
-  function showBoards() {}
-
   return (
     <header className={ _className }>
       <div className={ headerLogoClassName }>
         <img src={ darkMode ? logoLight : logoDark } alt="Header logo" />
       </div>
       <div className="header__informative">
-        <h2 className="header__informative-title" onClick={ showBoards }>{ pageTitle() }</h2>
+        <h2 className="header__informative-title">{ pageTitle() }</h2>
         <NavbarBtnGroup />
       </div>
     </header>
